88432a8a931a9d9fb99380be656ed8274d7d2f9e
CallMeRinChan <call0me0rin0chan@gmail.com>
2022-11-24 09:39:19 +0100

    initial commit

---
+++ AOC_2021/day_02.php
@@ -0,0 +1,23 @@
+<?php
+
+$file = file_get_contents("input_day02.txt");
+$nums = explode("\n", $file);
+
+$horizontal = 0;
+$depth = 0;
+$aim = 0;
+
+foreach($nums as $number => $_){
+    if (substr($nums[$number], 0, -2) == "forward") {
+        $horizontal += intval(substr($nums[$number],-1));
+        $depth += $aim * intval(substr($nums[$number],-1));
+    }
+    if (substr($nums[$number], 0, -2) == "up") {
+        $aim -= intval(substr($nums[$number],-1));
+    }
+    if (substr($nums[$number], 0, -2) == "down") {
+        $aim += intval(substr($nums[$number],-1));
+    }
+}
+
+echo "$horizontal * $depth = " . $horizontal * $depth;
