9affea3b9c636ad3c8736790f8f21fa455350fcf
CallMeRinChan <call0me0rin0chan@gmail.com>
2022-12-02 08:45:54 +0100

    day 2 both parts

--- AOC_2022/day_01_p2.php
+++ AOC_2022/day_01_p2.php
@@ -1,17 +1,9 @@
 <?php
 
-function debug($iets){
-    echo "<pre>";
-    print_r($iets);
-}
-
-
 ini_set("display_errors", 1);
 
 $file = file_get_contents("inputs/input_day1.txt");
 $inputs = explode("\n", $file);
-
-//var_dump($inputs);
 
 $elves = [];
 $currentElf = 0;
@@ -27,21 +19,6 @@ foreach($inputs as $input) // krijg elke input
         $elf++;
     }
 }
-//print_r($elves);
-//$elves = asort($elves);
-rsort($elves);
-//print_r($elves);
-echo $elves[0] + $elves[1] + $elves[2];
 
-/*      -- 3 elven met het meese krijgen
- *
- *  krijg elke input
- *  tel de input bij de currentElf op
- *  als huidige input empty is:
- *  doe currentElf in de elves array
- *  reset currentElf
- *  ga een Elf omhoog
- *
- *  sorteer de array
- *  tel de bovenste 3 op
- */
+rsort($elves);
+echo $elves[0] + $elves[1] + $elves[2];
---
+++ AOC_2022/day_02_p1.php
@@ -0,0 +1,90 @@
+<?php
+
+ini_set("display_errors", 1);
+
+// A = rock, B = paper, C is scissors
+// X = rock, Y = paper, Z is scissors
+
+// win gives you 6
+// draw gives you 3
+// loss gives you 0
+
+// rock gives you 1
+// paper gives you 2
+// scissors gives you 3
+
+$file = file_get_contents("inputs/input_day2.txt");
+$inputs = explode("\n", $file);
+
+$totalPoints = 0;
+
+foreach($inputs as $char)
+{
+    $opponent = substr($char, 0, 1);
+    $player = substr($char, -1, 1);
+
+    $totalPoints += doThrowPoints($player);
+    $totalPoints += doTurns($opponent,$player);
+
+
+}
+seeResults($totalPoints);
+
+
+function doThrowPoints($throw) : int{
+    $points = 0;
+    switch($throw){
+        case "X":
+            $points = 1;
+            break;
+        case "Y":
+            $points = 2;
+            break;
+        case "Z":
+            $points = 3;
+            break;
+    }
+    return $points;
+}
+
+function doTurns($opponent, $player) : int{
+    $points = 0;
+    if($opponent == "A"){
+        if($player == "X"){
+            $points = 3;
+        }
+        if($player == "Y"){
+            $points = 6;
+        }
+        if($player == "Z"){
+            $points = 0;
+        }
+    }
+    if($opponent == "B"){
+        if($player == "X"){
+            $points = 0;
+        }
+        if($player == "Y"){
+            $points = 3;
+        }
+        if($player == "Z"){
+            $points = 6;
+        }
+    }
+    if($opponent == "C"){
+        if($player == "X"){
+            $points = 6;
+        }
+        if($player == "Y"){
+            $points = 0;
+        }
+        if($player == "Z"){
+            $points = 3;
+        }
+    }
+    return $points;
+}
+
+function seeResults($value) : void{
+    echo $value;
+}
